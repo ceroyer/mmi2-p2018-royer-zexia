a53dbda0e782d4a5e3d9b73f1e3f3b02d051edc9
ceroyer <charles-eliott.royer@laposte.net>
2018-04-02 15:47:44 +0200

    responsive improvements

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,21 @@
+
 import { User } from './user';
 
 console.log('Welcome !');
 
 let name = 'Bastien';
 const user = new User(name);
+
+/*
+(function(){
+  var headerBurger = document.querySelector('.header__burger');
+
+  headerBurger.addEventListener("click", function(){
+    alert('yo');
+    headerBurger.classList.toggle('clicked');
+    document.querySelector('.header').classList.toggle('show');
+    document.querySelector('body').classList.toggle('overflow');
+  });
+
+})();
+*/
