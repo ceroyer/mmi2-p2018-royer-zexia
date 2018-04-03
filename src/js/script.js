aed89f84106896337fa6cb04c82490afe62c1614
Charly ROYER <charles-eliott.royer@laposte.net>
2018-04-03 20:51:25 +0200

    console.log

--- src/js/script.js
+++ src/js/script.js
@@ -30,5 +30,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   let $video = "<iframe src='https://www.youtube.com/embed/UyHmyLyBm_A' allowfullscreen></iframe>";
   document.getElementsByClassName('wrap__video')[0].innerHTML = $video;
+  console.log("OK");
 
 });
