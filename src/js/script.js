f6ae021ca92e9d5855a5b96e9feb88feecddddf1
Charly ROYER <charles-eliott.royer@laposte.net>
2018-04-03 20:26:57 +0200

    End score wrapper + responsive

--- src/js/script.js
+++ src/js/script.js
@@ -26,4 +26,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('.header').classList.toggle('header--show');
   });
 
+// DEFER YOUTUBE VIDEO PARSING
+
+  let $video = "<iframe src='https://www.youtube.com/embed/UyHmyLyBm_A' allowfullscreen></iframe>";
+  document.getElementsByClassName('wrap__video')[0].innerHTML = $video;
+
 });
